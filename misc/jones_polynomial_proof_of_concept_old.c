6504f54a4da13c3a2158dccc68bdc3f223417213
ryanmaguire <rmaguire@protonmail.com>
2023-11-01 17:30:42 -0400

    Update license.

--- misc/jones_polynomial_proof_of_concept_old.c
+++ misc/jones_polynomial_proof_of_concept_old.c
@@ -1,20 +1,20 @@
 /******************************************************************************
- *                                 LICENSE                                    *
+ *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is part of libtmpl.                                             *
+ *  This file is part of knot_data.                                           *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  knot_data is free software: you can redistribute it and/or modify         *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  libtmpl is distributed in the hope that it will be useful,                *
+ *  knot_data is distributed in the hope that it will be useful,              *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ *  along with knot_data.  If not, see <https://www.gnu.org/licenses/>.       *
  ******************************************************************************
  *  Attempt of a proof of concept of computing the Jones' polynomial from     *
  *  the Gauss code of a knot.                                                 *
